43ea31e55bfca459faf63e8cee60cda69e878d37
dinhphu28 <18110175@student.hcmute.edu.vn>
2021-05-10 13:26:47 +0700

    Init somethings

---
+++ BackEnd/smarthome/src/main/java/com/anthony/smarthome/SmarthomeApplication.java
@@ -0,0 +1,13 @@
+package com.anthony.smarthome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmarthomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmarthomeApplication.class, args);
+	}
+
+}
---
+++ BackEnd/smarthome/src/test/java/com/anthony/smarthome/SmarthomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anthony.smarthome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmarthomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
