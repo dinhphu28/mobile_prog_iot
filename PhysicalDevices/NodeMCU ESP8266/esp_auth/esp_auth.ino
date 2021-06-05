726b826f1430a9c4d569d9b32d3ee4adfbdf7e5a
dinhphu28 <18110175@student.hcmute.edu.vn>
2021-06-05 19:49:14 +0700

    Update Physical Device

--- PhysicalDevices/NodeMCU ESP8266/esp_auth/esp_auth.ino
+++ PhysicalDevices/NodeMCU ESP8266/esp_auth/esp_auth.ino
@@ -3,7 +3,7 @@
 #include <ESP8266HTTPClient.h>
 
 
-String host = "http://192.168.172.12:8080";//sửa host!
+String host = "http://192.168.172.5:8080/smarthome";//sửa host!
 String mytoken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsImlzcyI6ImF1dGgwIn0.f1qc-fvEuI-k_PErFZuLYYXcX8QejY6Cy25a9xdz0QE";
 // my token;
 
@@ -12,6 +12,8 @@ void setup () {
   Serial.begin(115200);
   WiFi.begin("Cafe 415.1", "JetBrain@LR300"); //sửa wifi!
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(4, OUTPUT);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting...");
@@ -31,14 +33,33 @@ void loop() {
      Serial.println(payload);
     }
 
-   DynamicJsonDocument doc(1024);
+    http.begin(host + "/api/v1/devices/2");  
+    http.addHeader("Authorization", "Bearer " + mytoken);
+    int httpCode2 = http.GET();                                  
+    String payload2;
+    if (httpCode2 > 0) {  
+     payload2 = http.getString();   
+     Serial.println(payload2);
+    }
+
+   DynamicJsonDocument doc(1024), doc2(1024);
    deserializeJson(doc, payload);
+   deserializeJson(doc2, payload2);
    bool bitValue = doc["bitValue"];
+   bool bitValue2 = doc2["bitValue"];
    Serial.println(bitValue);
    if(bitValue == true) {
       digitalWrite(LED_BUILTIN, LOW);
+      digitalWrite(5, HIGH);
    } else {
       digitalWrite(LED_BUILTIN, HIGH);
+      digitalWrite(5, LOW);
+   }
+
+   if(bitValue2 == true) {
+      digitalWrite(4, HIGH);
+   } else {
+      digitalWrite(4, LOW);
    }
   }
 //  delay(200);/
